54894f5b205cea9e8a416b3c1781704d06b706de
Not Mark <92692104+PocketEngineer1@users.noreply.github.com>
2022-12-27 18:25:32 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ else:
     config.read('config.ini')
     log('No config file found!', 2)
 
+# Language File Handler
 if os.path.exists('./lang/'+config['MAIN']['fallback_lang']+'.toml'):
     lang = toml.decoder.load('./lang/'+config['MAIN']['fallback_lang']+'.toml')
 else:
@@ -71,96 +72,100 @@ if os.path.exists('./lang/'+config['MAIN']['lang']+'.toml'):
     Merge(toml.decoder.load('./lang/'+config['MAIN']['lang']+'.toml'), lang)
 else:
     raise FileExistsError('Failed to locate language file')
+# Language File Handler
 
-def Help(command=''):
-    if command == '':
-        print(lang['HELP']['more_info']+'\n')
-        print('HELP           '+lang['HELP']['HELP']['main'])
-        print('CD             '+lang['HELP']['CD']['main'])
-        print('LS             '+lang['HELP']['LIST_DIR']['main'])
-        print('DIR            '+lang['HELP']['LIST_DIR']['main'])
-        print('CLS            '+lang['HELP']['CLEAR']['main'])
-        print('CLEAR          '+lang['HELP']['CLEAR']['main'])
-        print('READ           '+lang['HELP']['READ']['main'])
-        print('DUMP           '+lang['HELP']['READ']['main'])
-        print('HOST           '+lang['HELP']['HOST_CMD']['main'])
-        print('INTEG          '+lang['HELP']['PACKAGE']['main'])
-        print('PKG            '+lang['HELP']['PACKAGE']['main'])
-        print('EXIT           '+lang['HELP']['EXIT']['main'])
-        print('QUIT           '+lang['HELP']['EXIT']['main'])
-        print('DIE            '+lang['HELP']['EXIT']['main'])
-        print('END            '+lang['HELP']['EXIT']['main'])
-        print('ABORT          '+lang['HELP']['EXIT']['main'])
-        print('ENDCLI         '+lang['HELP']['EXIT']['main'])
-        print('ABANDON        '+lang['HELP']['EXIT']['main'])
-        print('SYS            '+lang['HELP']['SYSTEM']['main'])
-        print('SYSTEM         '+lang['HELP']['SYSTEM']['main'])
+class commands:
+    def Help(command=''):
+        if command == '':
+            print(lang['HELP']['more_info']+'\n')
+            print('HELP           '+lang['HELP']['HELP']['main'])
+            print('CD             '+lang['HELP']['CD']['main'])
+            print('LS             '+lang['HELP']['LIST_DIR']['main'])
+            print('DIR            '+lang['HELP']['LIST_DIR']['main'])
+            print('CLS            '+lang['HELP']['CLEAR']['main'])
+            print('CLEAR          '+lang['HELP']['CLEAR']['main'])
+            print('READ           '+lang['HELP']['READ']['main'])
+            print('DUMP           '+lang['HELP']['READ']['main'])
+            print('HOST           '+lang['HELP']['HOST_CMD']['main'])
+            print('INTEG          '+lang['HELP']['PACKAGE']['main'])
+            print('PKG            '+lang['HELP']['PACKAGE']['main'])
+            print('EXIT           '+lang['HELP']['EXIT']['main'])
+            print('QUIT           '+lang['HELP']['EXIT']['main'])
+            print('DIE            '+lang['HELP']['EXIT']['main'])
+            print('END            '+lang['HELP']['EXIT']['main'])
+            print('ABORT          '+lang['HELP']['EXIT']['main'])
+            print('ENDCLI         '+lang['HELP']['EXIT']['main'])
+            print('ABANDON        '+lang['HELP']['EXIT']['main'])
+            print('SYS            '+lang['HELP']['SYSTEM']['main'])
+            print('SYSTEM         '+lang['HELP']['SYSTEM']['main'])
 
-    elif command.upper() == 'HELP':
-        print(lang['HELP']['HELP']['main']+'\n')
+        elif command.upper() == 'HELP':
+            print(lang['HELP']['HELP']['main']+'\n')
 
-    elif command.upper() == 'CD':
-        print(lang['HELP']['CD']['main']+'\n')
+        elif command.upper() == 'CD':
+            print(lang['HELP']['CD']['main']+'\n')
 
-    elif command.upper() == 'LS':
-        print(lang['HELP']['LIST_DIR']['main']+'\n')
+        elif command.upper() == 'LS':
+            print(lang['HELP']['LIST_DIR']['main']+'\n')
 
-    elif command.upper() == 'DIR':
-        print(lang['HELP']['LIST_DIR']['main']+'\n')
+        elif command.upper() == 'DIR':
+            print(lang['HELP']['LIST_DIR']['main']+'\n')
 
-    elif command.upper() == 'CLS':
-        print(lang['HELP']['CLEAR']['main']+'\n')
+        elif command.upper() == 'CLS':
+            print(lang['HELP']['CLEAR']['main']+'\n')
 
-    elif command.upper() == 'CLEAR':
-        print(lang['HELP']['CLEAR']['main']+'\n')
+        elif command.upper() == 'CLEAR':
+            print(lang['HELP']['CLEAR']['main']+'\n')
 
-    elif command.upper() == 'READ':
-        print(lang['HELP']['READ']['main']+'\n')
+        elif command.upper() == 'READ':
+            print(lang['HELP']['READ']['main']+'\n')
 
-    elif command.upper() == 'DUMP':
-        print(lang['HELP']['READ']['main']+'\n')
+        elif command.upper() == 'DUMP':
+            print(lang['HELP']['READ']['main']+'\n')
 
-    elif command.upper() == 'HOST':
-        print(lang['HELP']['HOST_CMD']['main']+'\n')
+        elif command.upper() == 'HOST':
+            print(lang['HELP']['HOST_CMD']['main']+'\n')
 
-    elif command.upper() == 'INTEG':
-        print(lang['HELP']['PACKAGE']['main']+'\n')
-        print('INSTALL        '+lang['HELP']['PACKAGE']['SUB']['install'])
-        print('REMOVE         '+lang['HELP']['PACKAGE']['SUB']['remove'])
+        elif command.upper() == 'INTEG':
+            print(lang['HELP']['PACKAGE']['main']+'\n')
+            print('INSTALL        '+lang['HELP']['PACKAGE']['SUB']['install'])
+            print('REMOVE         '+lang['HELP']['PACKAGE']['SUB']['remove'])
 
-    elif command.upper() == 'PKG':
-        print(lang['HELP']['PACKAGE']['main']+'\n')
-        print('INSTALL        '+lang['HELP']['PACKAGE']['SUB']['install'])
-        print('REMOVE         '+lang['HELP']['PACKAGE']['SUB']['remove'])
+        elif command.upper() == 'PKG':
+            print(lang['HELP']['PACKAGE']['main']+'\n')
+            print('INSTALL        '+lang['HELP']['PACKAGE']['SUB']['install'])
+            print('REMOVE         '+lang['HELP']['PACKAGE']['SUB']['remove'])
 
-    elif command.upper() == 'EXIT':
-        print(lang['HELP']['EXIT']['main']+'\n')
+        elif command.upper() == 'EXIT':
+            print(lang['HELP']['EXIT']['main']+'\n')
 
-    elif command.upper() == 'QUIT':
-        print(lang['HELP']['EXIT']['main']+'\n')
+        elif command.upper() == 'QUIT':
+            print(lang['HELP']['EXIT']['main']+'\n')
 
-    elif command.upper() == 'DIE':
-        print(lang['HELP']['EXIT']['main']+'\n')
+        elif command.upper() == 'DIE':
+            print(lang['HELP']['EXIT']['main']+'\n')
 
-    elif command.upper() == 'END':
-        print(lang['HELP']['EXIT']['main']+'\n')
+        elif command.upper() == 'END':
+            print(lang['HELP']['EXIT']['main']+'\n')
 
-    elif command.upper() == 'ABORT':
-        print(lang['HELP']['EXIT']['main']+'\n')
+        elif command.upper() == 'ABORT':
+            print(lang['HELP']['EXIT']['main']+'\n')
 
-    elif command.upper() == 'ENDCLI':
-        print(lang['HELP']['EXIT']['main']+'\n')
+        elif command.upper() == 'ENDCLI':
+            print(lang['HELP']['EXIT']['main']+'\n')
 
-    elif command.upper() == 'ABANDON':
-        print(lang['HELP']['EXIT']['main']+'\n')
+        elif command.upper() == 'ABANDON':
+            print(lang['HELP']['EXIT']['main']+'\n')
 
-    elif command.upper() == 'SYS':
-        print(lang['HELP']['SYSTEM']['main']+'\n')
-        print('RECURSION      '+lang['HELP']['SYSTEM']['SUB']['recursion'])
+        elif command.upper() == 'SYS':
+            print(lang['HELP']['SYSTEM']['main']+'\n')
+            print('RECURSION      '+lang['HELP']['SYSTEM']['SUB']['recursion'])
 
-    elif command.upper() == 'SYSTEM':
-        print(lang['HELP']['SYSTEM']['main']+'\n')
-        print('RECURSION      '+lang['HELP']['SYSTEM']['SUB']['recursion'])
+        elif command.upper() == 'SYSTEM':
+            print(lang['HELP']['SYSTEM']['main']+'\n')
+            print('RECURSION      '+lang['HELP']['SYSTEM']['SUB']['recursion'])
+    # end of function
+# end of class
 
 def cmd():
     Dir = os.path.normpath(os.getcwd())
@@ -168,9 +173,9 @@ def cmd():
 
     if usr_in.lower().startswith('help'):
         if usr_in.lower().startswith('help '):
-            Help(re.split('help ', usr_in, 1, flags=re.IGNORECASE)[1])
+            commands.Help(re.split('help ', usr_in, 1, flags=re.IGNORECASE)[1])
         else:
-            Help()
+            commands.Help()
 
     elif usr_in.lower().startswith('cd'):
         if usr_in.lower().startswith('cd '):
@@ -179,7 +184,7 @@ def cmd():
             else:
                 log(lang['ERROR']['invalid_path'], 3)
         else:
-            Help('CD')
+            commands.Help('CD')
 
     elif usr_in.lower().startswith('ls'):
         if usr_in.lower().startswith('ls '):
@@ -203,25 +208,25 @@ def cmd():
         if usr_in.lower().startswith('sys '):
             log(lang['ERROR']['missing_command'], 4)
         else:
-            Help('SYS')
+            commands.Help('SYS')
     
     elif usr_in.lower().startswith('system'):
         if usr_in.lower().startswith('system '):
             log(lang['ERROR']['missing_command'], 4)
         else:
-            Help('SYSTEM')
+            commands.Help('SYSTEM')
 
     elif usr_in.lower().startswith('integ'):
         if usr_in.lower().startswith('integ '):
             log(lang['ERROR']['missing_command'], 4)
         else:
-            Help('INTEG')
+            commands.Help('INTEG')
 
     elif usr_in.lower().startswith('pkg'):
         if usr_in.lower().startswith('pkg '):
             log(lang['ERROR']['missing_command'], 4)
         else:
-            Help('PKG')
+            commands.Help('PKG')
 
     elif usr_in.lower().startswith('host'):
         log("It appears that this may not work correctly", 2)
@@ -229,7 +234,7 @@ def cmd():
             log(re.split('host ', usr_in, 1, flags=re.IGNORECASE)[1], Print=False)
             subprocess.run(re.split('host ', usr_in, 1, flags=re.IGNORECASE)[1].split(" "))
         else:
-            Help('HOST')
+            commands.Help('HOST')
 
     elif usr_in.lower().startswith('read'):
         if usr_in.lower().startswith('read '):
@@ -241,7 +246,7 @@ def cmd():
             else:
                 log(lang['ERROR']['invalid_path'], 3)
         else:
-            Help('READ')
+            commands.Help('READ')
 
     elif usr_in.lower().startswith('dump'):
         if usr_in.lower().startswith('dump '):
@@ -253,7 +258,7 @@ def cmd():
             else:
                 log(lang['ERROR']['invalid_path'], 3)
         else:
-            Help('DUMP')
+            commands.Help('DUMP')
             
     elif usr_in.lower().startswith('clear') or usr_in.lower().startswith('cls'):
         if os.name == 'nt':
@@ -274,6 +279,7 @@ def cmd():
         log(lang['ERROR']['unknown_command'], 3)
 
     cmd()
+# end of function
 
 print('   _____ _          _ _  \n  / ____| |        | | | \n | (___ | |__   ___| | | \n  \___ \| \'_ \ / _ \ | | \n  ____) | | | |  __/ | | \n |_____/|_| |_|\___|_|_| \n')
 print('Welcome to Shell! A command line shell created by Not Mark, because why not!\nNote: The command set is bit of everything.\n')
